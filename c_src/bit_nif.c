787645645ae98567eabfeb01443b0d18b7a9c245
Tony Rogvall <tony@rogvall.se>
2018-05-10 11:40:57 +0200

    added gcc builtins

--- c_src/bit_nif.c
+++ c_src/bit_nif.c
@@ -45,7 +45,58 @@ ErlNifFunc bit_funcs[] =
     { "count",  1, bit_count },
 };
 
-// Calculate size of representation
+
+#if defined(HALFWORD_HEAP_EMULATOR) || (SIZEOF_LONG == 4)
+#define B7 0x77777777
+#define B3 0x33333333
+#define B1 0x11111111
+#define BM 0x0F0F0F0F
+#elif (SIZEOF_LONG == 8)
+#define B7 0x7777777777777777LL
+#define B3 0x3333333333333333LL
+#define B1 0x1111111111111111LL
+#define BM 0x0F0F0F0F0F0F0F0FLL
+#else 
+#error "bad erl_int_size integer configuration"
+#endif
+
+#if SIZEOF_VOID_P == 8
+#define SIZE_OF_DIGIT 8
+#elif SIZEOF_VOID_P == 4
+#define SIZE_OF_DIGIT 4
+#else 
+#error "bad erl_int_size integer configuration"
+#endif
+
+#if (SIZEOF_LONG_LONG == SIZE_OF_DIGIT)
+#define POPCOUNT(x) __builtin_popcountll((x))
+#define PARITY(x)   __builtin_parityll((x))
+#define FFS(x)      __builtin_ffsll((x))
+#define CLZ(x)      __builtin_clzll((x))
+#define CTZ(x)      __builtin_ctzll((x))
+#elif (SIZEOF_LONG == SIZE_OF_DIGIT)
+#define POPCOUNT(x) __builtin_popcountl((x))
+#define PARITY(x)   __builtin_parityl((x))
+#define FFS(x)      __builtin_ffsl((x))
+#define CLZ(x)      __builtin_clzl((x))
+#define CTZ(x)      __builtin_ctzl((x))
+#elif (SIZEOF_INT == SIZE_OF_DIGIT)
+#define POPCOUNT(x) __builtin_popcount((x))
+#define PARITY(x)   __builtin_parity((x))
+#define FFS(x)      __builtin_ffs((x))
+#define CLZ(x)      __builtin_clz((x))
+#define CTZ(x)      __builtin_ctz((x))    
+#else
+static int POPCOUNT(ErlNifBigDigit x)
+{
+    x = (x) - (((x>>1)&B7) + ((x>>2)&B3) + ((x>>3)&B1));
+    x = x + (x>>4);
+    return ((x & BM) % 255);
+}
+#endif
+
+// Calculate size of representation, for negative numbers
+// the two complement form is used to obtain a reasonable size
 ERL_NIF_TERM bit_size(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     ErlNifBignum  big;
@@ -172,20 +223,6 @@ ERL_NIF_TERM bit_toggle(ErlNifEnv* env, int argc,  const ERL_NIF_TERM argv[])
     return enif_make_badarg(env);
 }
 
-#if defined(HALFWORD_HEAP_EMULATOR) || (SIZEOF_LONG == 4)
-#define B7 0x77777777
-#define B3 0x33333333
-#define B1 0x11111111
-#define BM 0x0F0F0F0F
-#elif (SIZEOF_LONG == 8)
-#define B7 0x7777777777777777LL
-#define B3 0x3333333333333333LL
-#define B1 0x1111111111111111LL
-#define BM 0x0F0F0F0F0F0F0F0FLL
-#else 
-#error "bad erl_int_size integer configuration"
-#endif
-
 ERL_NIF_TERM bit_count(ErlNifEnv* env, int argc,  const ERL_NIF_TERM argv[])
 {
     ErlNifBignum  big;
@@ -193,18 +230,10 @@ ERL_NIF_TERM bit_count(ErlNifEnv* env, int argc,  const ERL_NIF_TERM argv[])
     int i;
     if (!enif_get_number(env, argv[0], &big))
 	return enif_make_badarg(env);
-    i = 0;
-    count = 0;
-    while(i < big.size) {
-	ErlNifBigDigit d = big.digits[i];
-	d = (d) - (((d>>1)&B7) + ((d>>2)&B3) + ((d>>3)&B1));
-	d = d + (d>>4);
-	count += ((d & BM) % 255);
-	i++;
-    }
+    for (i=0; i<big.size;i++)
+	count += POPCOUNT(big.digits[i]);
     return enif_make_int(env, count);
 }
-
 
 static int bit_load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
 {
